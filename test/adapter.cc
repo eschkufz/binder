2ed1739017c258bf92aa26d8ac88011679b2f945
eric schkufza <eric.schkufza@gmail.com>
2017-02-07 14:34:10 -0800

    Moved all adapter tests out of cache test.

--- test/adapter.cc
+++ test/adapter.cc
@@ -41,3 +41,37 @@ TEST(adapter_store, backing_store) {
   EXPECT_FALSE(s.contains(2));
   EXPECT_TRUE(s.contains(1));
 }
+
+// Mapping tests
+TEST(adapter_store, one_to_one) {
+  struct M {
+    int kmap(int dk) { return dk + 1; }
+    int vmap(int dv) { return dv + 1; }
+    int kunmap(int rk) { return rk - 1; }
+    int vunmap(int rv) { return rv - 1; }
+    int vunmap(int dk, int rk, int rv) { return rv - 1; }
+  };
+  Store<int, int> ii;
+  AdapterStore<int,int,decltype(ii),M> s(&ii);
+
+  s.put(make_pair(1,1));
+  EXPECT_TRUE(s.contains(1));
+  EXPECT_EQ(s.get(1), 1);
+}
+TEST(adapter, many_to_one) {
+  struct M {
+    int kmap(int dk) { return dk % 2; }
+    int vmap(int dv) { return dv + 1; }
+    int kunmap(int rk) { return rk; }
+    int vunmap(int rv) { return rv - 1; }
+    int vunmap(int dk, int rk, int rv) { return rv - 1; }
+  };
+  Store<int, int> ii;
+  AdapterStore<int,int,decltype(ii),M> s(&ii);
+
+  s.put(make_pair(1,1));
+  s.put(make_pair(3,1));
+  EXPECT_EQ(s.size(), 1);
+  EXPECT_EQ(s.get(1), 1);
+  EXPECT_EQ(s.get(3), 1);
+}
